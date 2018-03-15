093a707a3ece277aeb5277cfc07f8610651a6e7e
Mrlyjoutlook <mr.lyj@outlook.com>
2018-03-15 14:35:39 +0800

    add ok http-cache examole

--- app/controllers/httpCache.js
+++ app/controllers/httpCache.js
@@ -9,7 +9,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
-const send = require("koa-send");
+const fs = require("fs-extra");
+const crypto = require('crypto');
 class HttpCache {
     index(ctx) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -20,15 +21,37 @@ class HttpCache {
     }
     cache(ctx) {
         return __awaiter(this, void 0, void 0, function* () {
-            yield send(ctx, ctx.params.name, {
-                root: path.resolve(__dirname, '../../static'),
-                setHeaders: (res, path, stats) => {
-                    console.log(path);
-                    console.log(stats);
-                    res.setHeader('Last-Modified', Date());
-                    res.setHeader('Cache-Control', '30');
+            let stats;
+            const rootPath = path.resolve(__dirname, '../../static', ctx.params.name);
+            if (ctx.params.name === 'li.json') {
+                stats = yield fs.stat(rootPath);
+                ctx.set('Content-Length', String(stats.size));
+                if (ctx.get('if-modified-since') &&
+                    ctx.get('if-modified-since') === stats.mtime.toUTCString()) {
+                    ctx.status = 304;
+                    return;
                 }
-            });
+                else {
+                    ctx.set('Last-Modified', stats.mtime.toUTCString());
+                    ctx.body = fs.createReadStream(rootPath);
+                }
+            }
+            if (ctx.params.name === 'ei.json') {
+                function getHash(str) {
+                    const chash = crypto.createHash('sha1');
+                    return chash.update(str).digest('base64');
+                }
+                const file = yield fs.readFile(rootPath);
+                const hash = getHash(file);
+                if (ctx.get('if-none-match') && ctx.get('if-none-match') === hash) {
+                    ctx.status = 304;
+                    return;
+                }
+                else {
+                    ctx.set('Etag', hash);
+                    ctx.body = fs.createReadStream(rootPath);
+                }
+            }
         });
     }
 }
--- src/controllers/httpCache.ts
+++ src/controllers/httpCache.ts
@@ -1,6 +1,7 @@
 import * as koa from 'koa';
 import * as path from 'path';
-import * as send from 'koa-send';
+import * as fs from 'fs-extra';
+const crypto = require('crypto');
 
 class HttpCache {
   async index(ctx: koa.Context) {
@@ -9,21 +10,44 @@ class HttpCache {
     });
   }
   async cache(ctx: koa.Context) {
-    // console.log(ctx.fresh);
-    // if (ctx.fresh) {
-    //   ctx.status = 304;
-    //   return;
-    // }
+    let stats;
+    const rootPath = path.resolve(__dirname, '../../static', ctx.params.name);
 
-    await send(ctx, ctx.params.name, {
-      root: path.resolve(__dirname, '../../static'),
-      setHeaders: (res, path, stats) => {
-        console.log(path);
-        console.log(stats);
-        res.setHeader('Last-Modified', Date());
-        res.setHeader('Cache-Control', '30');
+    // test Last-Modified/If-Modified-Since
+    if (ctx.params.name === 'li.json') {
+      stats = await fs.stat(rootPath);
+
+      ctx.set('Content-Length', String(stats.size));
+
+      if (
+        ctx.get('if-modified-since') &&
+        ctx.get('if-modified-since') === stats.mtime.toUTCString()
+      ) {
+        ctx.status = 304;
+        return;
+      } else {
+        ctx.set('Last-Modified', stats.mtime.toUTCString());
+        ctx.body = fs.createReadStream(rootPath);
       }
-    });
+    }
+
+    // test Etag/If-None-Match
+    if (ctx.params.name === 'ei.json') {
+      function getHash(str: any) {
+        const chash = crypto.createHash('sha1');
+        return chash.update(str).digest('base64');
+      }
+
+      const file = await fs.readFile(rootPath);
+      const hash = getHash(file);
+      if (ctx.get('if-none-match') && ctx.get('if-none-match') === hash) {
+        ctx.status = 304;
+        return;
+      } else {
+        ctx.set('Etag', hash);
+        ctx.body = fs.createReadStream(rootPath);
+      }
+    }
   }
 }
 
